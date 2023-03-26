49c2d5916aa5556d986152067126aa70b7db5b9b
Mateus Nascimento <mateus.msnascimento@gmail.com>
2023-03-26 20:38:02 -0300

    feat: add code to run graphile

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,8 @@
 import express from 'express'
+import postgraphile from 'postgraphile'
+
+
+const pgUser = 'admin'
 
 /**
 * This is our main entry point of our Express server.
@@ -7,6 +11,11 @@ import express from 'express'
 const App = () => {
  const app = express()
  app.use(express.json())
+ app.use(postgraphile(`postgresql://${pgUser}:${pgUser}@localhost/graphqltest_db`, 'public', {
+   watchPg: true,
+   graphiql: true,
+   enhanceGraphiql: true,
+ }))
 
  app.get('/api/v1/hello', async (req, res, next) => {
    res.send('success')
